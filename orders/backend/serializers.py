9abab9e335c755a58defa4499f66d9336c1757a8
ebogdanov <ebogdanov@msk.rsvo.ru>
2023-02-03 14:58:09 +0300

    Добить пару хвостов.

--- orders/backend/serializers.py
+++ orders/backend/serializers.py
@@ -1,7 +1,8 @@
 # Верстальщик
 from rest_framework import serializers
 
-from .models import User, Contact#, Category, Shop, ProductInfo, Product, ProductParameter, OrderItem, Order
+from .models import User, Contact, ProductInfo, Product, ProductParameter\
+    , Category, Shop, OrderItem, Order
 
 
 class ContactSerializer(serializers.ModelSerializer):
@@ -19,71 +20,71 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'first_name', 'last_name', 'email', 'company', 'position', 'contacts')
+        fields = ('id', 'first_name', 'last_name', 'email', 'company', 'position', 'contacts', 'type', 'active')
         read_only_fields = ('id',)
 
 
-# class CategorySerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Category
-#         fields = ('id', 'name',)
-#         read_only_fields = ('id',)
-#
-#
-# class ShopSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Shop
-#         fields = ('id', 'name', 'state',)
-#         read_only_fields = ('id',)
-#
-#
-# class ProductSerializer(serializers.ModelSerializer):
-#     category = serializers.StringRelatedField()
-#
-#     class Meta:
-#         model = Product
-#         fields = ('name', 'category',)
-#
-#
-# class ProductParameterSerializer(serializers.ModelSerializer):
-#     parameter = serializers.StringRelatedField()
-#
-#     class Meta:
-#         model = ProductParameter
-#         fields = ('parameter', 'value',)
-#
-#
-# class ProductInfoSerializer(serializers.ModelSerializer):
-#     product = ProductSerializer(read_only=True)
-#     product_parameters = ProductParameterSerializer(read_only=True, many=True)
-#
-#     class Meta:
-#         model = ProductInfo
-#         fields = ('id', 'model', 'product', 'shop', 'quantity', 'price', 'price_rrc', 'product_parameters',)
-#         read_only_fields = ('id',)
-#
-#
-# class OrderItemSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = OrderItem
-#         fields = ('id', 'product_info', 'quantity', 'order',)
-#         read_only_fields = ('id',)
-#         extra_kwargs = {
-#             'order': {'write_only': True}
-#         }
-#
-#
-# class OrderItemCreateSerializer(OrderItemSerializer):
-#     product_info = ProductInfoSerializer(read_only=True)
-#
-#
-# class OrderSerializer(serializers.ModelSerializer):
-#     ordered_items = OrderItemCreateSerializer(read_only=True, many=True)
-#
-#     total_sum = serializers.IntegerField()
-#     contact = ContactSerializer(read_only=True)
-#
-#     class Meta:
-#         model = Order
-#         fields = ('id', 'ordered_items', 'state', 'dt', 'total_sum', 'contact',)
-#         read_only_fields = ('id',)
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id', 'name',)
+        read_only_fields = ('id',)
+
+
+class ShopSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Shop
+        fields = ('id', 'name', 'state',)
+        read_only_fields = ('id',)
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = serializers.StringRelatedField()
+
+    class Meta:
+        model = Product
+        fields = ('name', 'category',)
+
+
+class ProductParameterSerializer(serializers.ModelSerializer):
+    parameter = serializers.StringRelatedField()
+
+    class Meta:
+        model = ProductParameter
+        fields = ('parameter', 'value',)
+
+
+class ProductInfoSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+    product_parameters = ProductParameterSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = ProductInfo
+        fields = ('id', 'model', 'product', 'shop', 'quantity', 'price', 'price_rrc', 'product_parameters',)
+        read_only_fields = ('id',)
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = ('id', 'product_info', 'quantity', 'order',)
+        read_only_fields = ('id',)
+        extra_kwargs = {
+            'order': {'write_only': True}
+        }
+
+
+class OrderItemCreateSerializer(OrderItemSerializer):
+    product_info = ProductInfoSerializer(read_only=True)
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    ordered_items = OrderItemCreateSerializer(read_only=True, many=True)
+
+    total_sum = serializers.IntegerField()
+    contact = ContactSerializer(read_only=True)
+
+    class Meta:
+        model = Order
+        fields = ('id', 'ordered_items', 'state', 'dt', 'total_sum', 'contact',)
+        read_only_fields = ('id',)
--- orders/backend/urls.py
+++ orders/backend/urls.py
@@ -2,29 +2,28 @@ from django.urls import path, include
 
 from django_rest_passwordreset.views import reset_password_request_token, reset_password_confirm
 
-from .views import RegisterAccount, ConfirmAccount, LoginAccount, AccountDetails, ContactView #  PartnerUpdate, LoginAccount, CategoryView,
-#ShopView, ProductInfoView, BasketView, ContactView, OrderView, PartnerState, PartnerOrders
+from .views import RegisterAccount, ConfirmAccount, LoginAccount, AccountDetails, ContactView, \
+    PartnerUpdate, PartnerState , LoginAccount, CategoryView,   \
+    ShopView, ProductInfoView, BasketView, ContactView, OrderView, PartnerOrders
 
 
 app_name = 'backend'
 urlpatterns = [
-    path('user/register', RegisterAccount.as_view(), name='user-register'),
-    path('user/register/confirm', ConfirmAccount.as_view(), name='user-register-confirm'),
-    path('user/login', LoginAccount.as_view(), name='user-login'),
-    path('user/details', AccountDetails.as_view(), name='user-details'),    #
-    path('user/contact', ContactView.as_view(), name='user-contact'),
-    # path('partner/update', PartnerUpdate.as_view(), name='partner-update'),
-    # path('partner/state', PartnerState.as_view(), name='partner-state'),
-    # path('partner/orders', PartnerOrders.as_view(), name='partner-orders'),
+    path('user/register', RegisterAccount.as_view(), name='user-register'), #работает
+    path('user/register/confirm', ConfirmAccount.as_view(), name='user-register-confirm'),  #выдает ложное
+    path('user/login', LoginAccount.as_view(), name='user-login'),  #работает
+    path('user/details', AccountDetails.as_view(), name='user-details'),    #Работает
+    path('user/contact', ContactView.as_view(), name='user-contact'),   #Работает
+    path('user/password_reset', reset_password_request_token, name='password-reset'),   #Шевелится, проверить через почту
+    path('user/password_reset/confirm', reset_password_confirm, name='password-reset-confirm'), #Страница не найдена
 
+    path('partner/update', PartnerUpdate.as_view(), name='partner-update'),  #Работает
+    path('partner/state', PartnerState.as_view(), name='partner-state'),  #Работает
+    path('partner/orders', PartnerOrders.as_view(), name='partner-orders'),  #Работает
 
-    # path('user/contact', ContactView.as_view(), name='user-contact'),
-
-    # path('user/password_reset', reset_password_request_token, name='password-reset'),
-    # path('user/password_reset/confirm', reset_password_confirm, name='password-reset-confirm'),
-    # path('categories', CategoryView.as_view(), name='categories'),
-    # path('shops', ShopView.as_view(), name='shops'),
-    # path('products', ProductInfoView.as_view(), name='shops'),
-    # path('basket', BasketView.as_view(), name='basket'),
-    # path('order', OrderView.as_view(), name='order'),
+    path('shops', ShopView.as_view(), name='shops'),  #Работает
+    path('categories', CategoryView.as_view(), name='categories'),
+    path('products', ProductInfoView.as_view(), name='shops'),
+    path('basket', BasketView.as_view(), name='basket'),
+    path('order', OrderView.as_view(), name='order'),
 ]
